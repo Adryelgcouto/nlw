6ec42e271b73a5036ec6f8300306f497a4b0f491
adryelgcouto <adryelgcouto@gmail.com>
2022-04-03 23:59:49 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+//DOM Documente object Model
+/*Opens and closes the menu when clicking the icon: Hamburger and X*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+/*When you click a menu item, hide the menu*/
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/* Change the page header when der SCROLL */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // Scroll is larger than the height of the header
+    header.classList.add('scroll')
+  } else {
+    // smaller than the height of the header
+    header.classList.remove('scroll')
+  }
+})
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+const carousel 
